bcd2fbfc4f814466c707c0d1a86a3bde56752802
Tolkinxon <krosstony639@gmail.com>
2023-02-27 23:15:26 +0500

    added option

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,10 @@
-import Input from '../components/Input'
 import Table from '../components/Table'
 import TableHead from '../components/TableHead'
 import TableBody from '../components/TableBody'
 import TableRow from '../components/TableRow'
 import Box from '../components/Box'
 import '../App.css'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useContext } from 'react'
 import { items } from '../reducer/provider'
 import { Link } from 'react-router-dom'
@@ -33,11 +32,13 @@ function Main() {
 
   return (
     <>
-      <h1>
-        React CRUD application using JSON Server and useContext with useReducer
-      </h1>
-      <button>ADD USER</button>
       <div className="App">
+        <h1 className="main-header">
+          React CRUD application using JSON Server and useContext with
+          useReducer
+        </h1>
+        <button className="add-user">ADD USER</button>
+
         <Table>
           <TableHead>
             <div className="hrow">
@@ -46,7 +47,7 @@ function Main() {
               <Box>Amount of products</Box>
               <Box>Protein</Box>
               <Box>Period of storage</Box>
-              <Box></Box>
+              <div className="space"></div>
             </div>
           </TableHead>
           <TableBody>
@@ -57,6 +58,8 @@ function Main() {
                 <Box>{row.amount}</Box>
                 <Box>{row.protein}</Box>
                 <Box>{row.storage}</Box>
+                <button>edit</button>
+
                 <button
                   onClick={() => {
                     delee(row.id)
@@ -71,12 +74,19 @@ function Main() {
         </Table>
         <div className="showing-page-bar">
           <p>Showing 5 of 6 products</p>
+          <label htmlFor="1">Show</label>
           <select name="amount_products" id="1">
             <option value="4">4</option>
-            <option value="4">4</option>
-            <option value="4">4</option>
-            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
           </select>
+          <div>
+            <i></i>
+            <span>1</span>
+            <span>2</span>
+            <i></i>
+          </div>
         </div>
       </div>
     </>
